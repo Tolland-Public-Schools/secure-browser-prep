58e98a784c2edb14ba4c9e3d7633dc430e3a95ea
Adam Sher <85632031+AdamAtTolland@users.noreply.github.com>
2025-04-04 10:27:05 -0400

    Initial code upload

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SecureBrowserPrep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Closing apps that prevent the Secure Browsering from running");
+            List<Process> allProcess = new List<Process>(Process.GetProcesses());
+            List<Process> toKill =  new List<Process>();
+
+            allProcess
+                .Where(x => x.ProcessName.ToLower()
+                    .StartsWith("teams"))
+                .ToList()
+                .ForEach(x => toKill.Add(x));
+            allProcess
+                .Where(x => x.ProcessName.ToLower()
+                    .StartsWith("teamviewer"))
+                .ToList()
+                .ForEach(x => toKill.Add(x));
+            allProcess
+                .Where(x => x.ProcessName.ToLower()
+                    .StartsWith("smart"))
+                .ToList()
+                .ForEach(x => toKill.Add(x));
+            allProcess
+                .Where(x => x.ProcessName.ToLower()
+                    .StartsWith("notebook"))
+                .ToList()
+                .ForEach(x => toKill.Add(x));
+            allProcess
+                .Where(x => x.ProcessName.ToLower()
+                    .StartsWith("chrome"))
+                .ToList()
+                .ForEach(x => toKill.Add(x));
+            allProcess
+                .Where(x => x.ProcessName.ToLower()
+                    .StartsWith("firefox"))
+                .ToList()
+                .ForEach(x => toKill.Add(x));
+            allProcess
+                .Where(x => x.ProcessName.ToLower()
+                    .StartsWith("ms-teams"))
+                .ToList()
+                .ForEach(x => toKill.Add(x));
+            allProcess
+                .Where(x => x.ProcessName.ToLower()
+                    .StartsWith("snippingtool"))
+                .ToList()
+                .ForEach(x => toKill.Add(x));
+            allProcess
+                .Where(x => x.ProcessName.ToLower()
+                    .StartsWith("taskmgr"))
+                .ToList()
+                .ForEach(x => toKill.Add(x));
+
+            foreach (Process p in toKill)
+            {
+                try
+                {
+                    Console.WriteLine("Closing {0}", p.ProcessName);
+                    p.Kill();
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+
+            // foreach (var process in Process.GetProcessesByName("Teams"))
+            // {
+            //     process.Kill();
+            // }
+            Console.WriteLine("All done");
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,7 @@
+To create a .exe file, run: dotnet publish -c Release -r win10-x64
+Open included .aip file in Advanced Installer
+Click Build to create .msi file in SecureBrowserPrepInstaller folder
+Run .\IntuneWinAppUtil.exe -c .\SecureBrowserPrepInstaller\ -s SecureBrowserPrep.msi -o .
+That will create the intunewin file you can upload to Intune
+
+If you have errors that System.Object is not imported, set VS Code mono option to auto in VS Code settings
